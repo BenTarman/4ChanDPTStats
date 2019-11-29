d33df8b00da8536d86a2980e6aa15bcd0aca0615
Eango <lordeango@gmail.com>
2019-11-29 14:21:42 -0700

    styled the 4chan thread a bit.

--- client/src/components/threadPost/component.js
+++ client/src/components/threadPost/component.js
@@ -1,7 +1,19 @@
 const ThreadPosts = Vue.component('thread-post', {
   props: ['post'],
 
-  template: threadPostTemplate
+  template: threadPostTemplate,
+
+  computed: {
+    // Will style post different if its the OP
+    postStyle: function() {
+      let cssClass = 'OP-post';
+      if (!this.post.replies) {
+        cssClass = 'thread-post';
+      }
+
+      return cssClass;
+    }
+  }
 });
 
 export default ThreadPosts;
